9cb792be6ca9e43460b0c9d6d51eb62de6370f4a
bryan.besnyi <besnyib@gmail.com>
2018-01-26 19:40:40 -0800

    initial commit

---
+++ src/main/java/com/cis2886/demo/SeniorProject2886Application.java
@@ -0,0 +1,12 @@
+package com.cis2886.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorProject2886Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorProject2886Application.class, args);
+	}
+}
